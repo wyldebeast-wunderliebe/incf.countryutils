78950b3704f7457ec3c1d67e1d1b26f939f607af
Huub Bouma <huub@pythonunited.com>
2025-02-26 12:23:35 +0100

    moved from setup.py to pyproject.toml

--- incf/countryutils/__init__.py
+++ incf/countryutils/__init__.py
@@ -1,1 +1,9 @@
-#
+"""incf.countryutils: A convenience API for country code transformations."""
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("incf.countryutils")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
